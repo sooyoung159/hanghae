919afcc64d818c8ce36eb010b645f653f96f7fb7
sooyoung <sooyoung159@naver.com>
2024-03-31 17:25:44 +0900

    과제 A

---
+++ chapter3/src/App.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Helmet} from "react-helmet-async";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Helmet>
+        <title>강수영</title>
+        <meta property="og:title" content="chapter3"/>
+        <meta property="og:description" content="설명"/>
+        <meta property="twitter:image" content=""/>
+        <meta name="twitter:card" content=""/>
+        <meta name="twitter:site" content=""/>
+        <meta name="twitter:title" content=""/>
+        <meta name="twitter:description" content=""/>
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+        <meta name="theme-color" content="#ffffff"/>
+      </Helmet>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo"/>
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
